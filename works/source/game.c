e9d7124dd82d1e0da76e2ba31d95a5c126e1b096
mxinshangshang <jiangmingxin825@live.cn>
2015-05-24 19:03:09 +0800

    use sscanf to slip the string success

--- works/source/game.c
+++ works/source/game.c
@@ -16,17 +16,6 @@ time_t now;
 struct tm *curTime;
 char filename[256];
 
-char hold[] = "hold/(.*)/hold";
-char flop[] = "flop/(.*)/flop";
-char turn[] = "turn/(.*)/turn";
-char river[] = "river/(.*)/river";
-regex_t reg;
-size_t  len;
-int err;
-char errbuf[1024];
-int nm=10;
-regmatch_t pmatch[10];
-
 char* CARDS[13]={"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
 int REPEAT[13]={0,0,0,0,0,0,0,0,0,0,0,0,0};
 char* Cards[7];
@@ -42,50 +31,32 @@ int fold=0;
 int on_server_message(int length, const char* buffer)
 {
     char reg_msg[50] = {'\0'};
-    char *buf;
-    char *result = NULL;
+    char buf[128] = {0};
+    char buf1[128] = {0};
+    char buf2[128] = {0};
+    char buf3[128] = {0};
+    char buf4[128] = {0};
+    char buf5[128] = {0};
+    char buf6[128] = {0};
+    char buf7[128] = {0};
+    char buf8[128] = {0};
+    char buf9[128] = {0};
+    char buf10[128] = {0};
+    char buf11[128] = {0};
+    char buf12[128] = {0};
+    char buf13[128] = {0};
+    char buf14[128] = {0};
     char dest[100] = {0};
     char *p, *p1, *p2;
     int i,j;
     int dui=0;
     DataFServer=fopen(filename,"a+");
-    printf("Server:\n%s\n", buffer);
-    fprintf(DataFServer,"____________________________________________Server\n%s\n", buffer);
-    
-    /*if(regcomp(&reg,hold,REG_EXTENDED)<0)
+    //fprintf(DataFServer,"____________________________________________Server\n%s\n", buffer);
+    if(strstr(buffer,"seat")!=NULL)         //  hold 
     {
-        regerror(err,&reg,errbuf,sizeof(errbuf));
-        printf("error: regcomp:%s\n",errbuf);
-    }
-    err = regexec(&reg,buffer,(size_t)nm,pmatch,0);
-    if(err == REG_NOMATCH)
-    {
-        printf("no match\n");
-        //exit(-1);
-    }
-    else if(err)
-    {
-        regerror(err,&reg,errbuf,sizeof(errbuf));
-        printf("err:%s\n",errbuf);
-        //exit(-1);
+       fprintf(DataFServer,"____________________________________________Server\n");
     }    
 
-    for(i=0;i<10 && pmatch[i].rm_so!=-1;i++)
-    {
-       int len;
-       len = pmatch[0].rm_eo-pmatch[0].rm_so;
-       if(len!=0)
-       {
-          memset(dest,'\0',sizeof(dest));
-          memcpy(dest,buffer+pmatch[0].rm_so,len);
-          //printf("%s\n",match);
-          //printf("Recieve Data From Server___________\n%s\n", dest);
-          fprintf(DataFServer,"__________hold:\n%s\n", dest);
-       }
-    }
-    regfree(&reg);*/
-
-    
 
     if(strstr(buffer,"hold")!=NULL)         //  hold 
     {
@@ -99,19 +70,16 @@ int on_server_message(int length, const char* buffer)
         {
             p1 += strlen("hold/\n");
             memcpy(dest, p1, p2 - p1);
-            result = strtok(dest,"\n");
-            while( result != NULL ) 
-            {
-               Cards[n++]=result;
-               fprintf(DataFServer,"%s", Cards[n-1]);
-               result = strtok( NULL,"\n");
-            }
-            color[0]=strtok(Cards[0]," ");        //第一张手牌
-            num[0]=strtok(NULL," ");
-            color[1]=strtok(Cards[1]," ");        //第二张手牌
-            num[1]=strtok(NULL," ");
-            //fprintf(DataFServer,"%s %s %s %s\n", color[0], num[0], color[1], num[1]); 
+            sscanf(dest,"%[^ ]%*s %*s %*s",buf1);
+            color[0]=buf1;
+            sscanf(dest,"%*s %[^ ]%*s %*s",buf2);
+            num[0]=buf2;
+            sscanf(dest,"%*s %*s %[^ ] %*s",buf3);
+            color[1]=buf3;
+            sscanf(dest,"%*s %*s %*s %[^ ]",buf4);
+            num[1]=buf4;
             //printf("hold:%s %s %s %s\n", color[0], num[0], color[1], num[1]);
+            fprintf(DataFServer,"hold:%s %s %s %s\n", color[0], num[0], color[1], num[1]);
             check=1;
         }    
     }
@@ -127,21 +95,20 @@ int on_server_message(int length, const char* buffer)
         {
             p1 += strlen("flop/\n");
             memcpy(dest, p1, p2 - p1);
-            result = strtok(dest,"\n");
-            while( result != NULL ) 
-            {
-               Cards[n++]=result;
-               fprintf(DataFServer,"%s", Cards[n-1]);
-               result = strtok( NULL,"\n");
-            } 
-            color[2]=strtok(Cards[2]," ");            //第三张手牌
-            num[2]=strtok(NULL," ");
-            color[3]=strtok(Cards[3]," ");            //第四张手牌
-            num[3]=strtok(NULL," ");
-            color[4]=strtok(Cards[4]," ");            //第五张手牌
-            num[4]=strtok(NULL," ");
-            //fprintf(DataFServer,"%s %s %s %s %s %s\n", color[2], num[2], color[3], num[3], color[4], num[4]);
-            printf("flop:%s %s %s %s %s %s\n", color[2], num[2], color[3], num[3], color[4], num[4]); 
+            sscanf(dest,"%[^ ]%*s %*s %*s %*s %*s",buf5);
+            color[2]=buf5;
+            sscanf(dest,"%*s %[^ ]%*s %*s %*s %*s",buf6);
+            num[2]=buf6;
+            sscanf(dest,"%*s %*s %[^ ] %*s %*s %*s",buf7);
+            color[3]=buf7;
+            sscanf(dest,"%*s %*s %*s %[^ ] %*s %*s",buf8);
+            num[3]=buf8;
+            sscanf(dest,"%*s %*s %*s %*s %[^ ] %*s",buf9);
+            color[4]=buf9;
+            sscanf(dest,"%*s %*s %*s %*s %*s %[^ ]",buf10);
+            num[4]=buf10;
+            //printf("flop:%s %s %s %s %s %s\n", color[2], num[2], color[3], num[3], color[4], num[4]); 
+            fprintf(DataFServer,"flop:%s %s %s %s %s %s\n", color[2], num[2], color[3], num[3], color[4], num[4]);
             for(i=0;i<13;i++)
             {
                 for(j=i+1;j<5;j++)
@@ -160,7 +127,7 @@ int on_server_message(int length, const char* buffer)
                    REPEAT[i]=0;                  
                 }
             }
-            printf("----------------5re_num:\n%d\n", re_num);
+            printf("----------------5re_num:%d\n", re_num);
             if(re_num>=2)
             {
                 call=0; 
@@ -188,17 +155,12 @@ int on_server_message(int length, const char* buffer)
         {
             p1 += strlen("turn/\n");
             memcpy(dest, p1, p2 - p1);
-            result = strtok(dest,"\n");
-            while( result != NULL ) 
-            {
-               Cards[n++]=result;
-               fprintf(DataFServer,"%s", Cards[n-1]);
-               result = strtok( NULL,"\n");
-            }
-            color[5]=strtok(Cards[5]," ");            //第六张手牌
-            num[5]=strtok(NULL," "); 
-            //fprintf(DataFServer,"%s %s\n", color[5], num[5]);
-            printf("turn:%s %s\n", color[5], num[5]);
+            sscanf(dest,"%[^ ]%*s",buf11);
+            color[5]=buf11;
+            sscanf(dest,"%*s %[^ ]",buf12);
+            num[5]=buf12;
+            //printf("turn:%s %s\n", color[5], num[5]);
+            fprintf(DataFServer,"turn:%s %s\n", color[5], num[5]);
             for(i=0;i<13;i++)
             {
                 for(j=i+1;j<6;j++)
@@ -218,7 +180,7 @@ int on_server_message(int length, const char* buffer)
                    REPEAT[i]=0;                  
                 }
             }
-            printf("----------------6re_num:\n%d\n", re_num);
+            printf("----------------6re_num:%d\n", re_num);
             if(re_num>=2)
             {
                 call=0; 
@@ -246,17 +208,12 @@ int on_server_message(int length, const char* buffer)
         {
             p1 += strlen("river/\n");
             memcpy(dest, p1, p2 - p1);
-            result = strtok(dest,"\n");
-            while( result != NULL ) 
-            {
-               Cards[n++]=result;
-               fprintf(DataFServer,"%s", Cards[n-1]);
-               result = strtok( NULL,"\n");
-            }
-            color[6]=strtok(Cards[6]," ");            //第七张手牌
-            num[6]=strtok(NULL," "); 
-            //fprintf(DataFServer,"%s %s\n", color[6], num[6]); 
-            printf("river:%s %s\n", color[6], num[6]);
+            sscanf(dest,"%[^ ]%*s",buf13);
+            color[6]=buf13;
+            sscanf(dest,"%*s %[^ ]",buf14);
+            num[6]=buf14;
+            //printf("river:%s %s\n", color[6], num[6]);
+            fprintf(DataFServer,"river:%s %s\n", color[6], num[6]);
             for(i=0;i<13;i++)
             {
                 for(j=i+1;j<7;j++)
@@ -276,7 +233,7 @@ int on_server_message(int length, const char* buffer)
                    REPEAT[i]=0;                  
                 }
             }
-            printf("----------------7re_num:\n%d\n", re_num);
+            printf("----------------7re_num:%d\n", re_num);
             if(re_num>=2)
             {
                 call=0; 
